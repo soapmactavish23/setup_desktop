ef4c608d49a035fe5732e773a1af145394b63011
henrick.nogueira <ricknogueira1231@gmail.com>
2025-08-19 00:03:03 -0300

    Acessando o navegador

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ load_dotenv()
 
 SENHA_NOTION = os.getenv("SENHA_NOTION")
 SENHA_GOOGLE = os.getenv("SENHA_GOOGLE")
+
+# abrir o chrome e daixar na página de busca do google
+clica_na_imagem('icone_chrome')
+sleep(1)
+pyautogui.hotkey("ctrl", "shift", "n")
+sleep(2)
+pyautogui.hotkey("ctrl", "l")
+pyautogui.write("google", interval=0.10)
+sleep(1)
+pyautogui.press("space")
+pyautogui.press("backspace")
+pyautogui.press("enter")
+sleep(2)
+clica_na_imagem("icone_google")
+sleep(4)
